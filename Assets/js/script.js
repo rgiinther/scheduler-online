806c73e9a3875e3975f5407b7f5368f9c6c78dda
rgiinther <r.giin95@gmail.com>
2022-04-19 22:16:57 -0500

    fixed local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,19 +1,20 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
     //display current day & time. (moment.js)
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
 
     $(".btn").on("click", function () {
         //get nearby values.
         console.log(this);
-        var text = $(this).siblings(".info").val(); // taken the change from the sibling html description attribute
-        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+        var text = $(this).siblings(".info").val(); //html attribute
+        var time = $(this).parent().attr("id");  //html id attribute
 
         //set items in local storage.
         localStorage.setItem(time, text);
     })
-})
+})*/
+
 //load data from local storage for each timeblock.
-$("#9 .info").val(localStorage.getItem("9"));
+/*$("#9 .info").val(localStorage.getItem("9"));
 $("#10 .info").val(localStorage.getItem("10"));
 $("#11 .info").val(localStorage.getItem("11"));
 $("#12 .info").val(localStorage.getItem("12"));
@@ -48,5 +49,112 @@ function scheduleTrack() {
             $(this).addClass("future");
         }
     })
+
+scheduleTrack(); 
+}*/
+
+
+//timer 
+var currentDay = moment();
+$("#currentDay").text(currentDay.format("MMMM Do YYYY, h:mm:ss a"));
+
+var currentHour = Number(currentDay.format('H'));
+
+setInterval(function() {
+    currentDay = moment();
+    $("#currentDay").text(currentDay.format("MMMM Do YYYY h:mm:ss a"));
+}, 1000)
+
+//local storage lines 69 to 
+//9am
+$("#btn0").click(function(){
+    var input9am = document.getElementById("0").value
+    localStorage.setItem("9am", JSON.stringify(input9am))
+})
+var output9am = JSON.parse(localStorage.getItem("9am"))
+document.getElementById("0").value = output9am;
+
+//10am
+$("#btn1").click(function(){
+    var input10am = document.getElementById("1").value
+    localStorage.setItem("10am", JSON.stringify(input10am))
+})
+var output10am = JSON.parse(localStorage.getItem("10am"))
+document.getElementById("1").value = output10am;
+
+//11am
+$("btn2").click(function(){
+    var input11am = document.getElementById("2").value
+    localStorage.setItem("11am", JSON.stringify(input11am))
+})
+var output11am = JSON.parse(localStorage.getItem("11am"))
+document.getElementById("2").value = output11am;
+
+//12pm
+$("#btn3").click(function(){
+    var input12pm = document.getElementById("3").value
+    localStorage.setItem("3", JSON.stringify(input12pm))
+})
+var output12pm = JSON.parse(localStorage.getItem("12pm"))
+document.getElementById("3").value = output12pm;
+
+//1pm
+$("#btn4").click(function(){
+    var input1pm = document.getElementById("4").value
+    localStorage.setItem("4", JSON.stringify(input12pm))
+})
+var output1pm = JSON.parse(localStorage.getItem("1pm"))
+document.getElementById("4").value = output1pm;
+
+//2pm
+$("#btn5").click(function(){
+    var input2pm = document.getElementById("5").value
+    localStorage.setItem("5", JSON.stringify(input12pm))
+})
+var output2pm = JSON.parse(localStorage.getItem("2pm"))
+document.getElementById("5").value = output2pm;
+
+//3pm
+$("#btn6").click(function(){
+    var input3pm = document.getElementById("6").value
+    localStorage.setItem("6", JSON.stringify(input3pm))
+})
+var output3pm = JSON.parse(localStorage.getItem("3pm"))
+document.getElementById("6").value = output3pm;
+
+//4pm
+$("#btn7").click(function(){
+    var input4pm = document.getElementById("7").value
+    localStorage.setItem("7", JSON.stringify(input4pm))
+})
+var output4pm = JSON.parse(localStorage.getItem("4pm"))
+document.getElementById("7").value = output4pm;
+
+//5pm
+$("#btn8").click(function(){
+    var input5pm = document.getElementById("8").value
+    localStorage.setItem("8", JSON.stringify(input12pm))
+})
+var output5pm = JSON.parse(localStorage.getItem("5pm"))
+document.getElementById("8").value = output5pm;
+
+
+
+//schedule tracking lines 96 to 
+//9am
+if (currentHour === 9) {
+    $("#0").addClass("present");
 }
-scheduleTrack(); //re-run function
+else if (currentHour > 9) {
+    $("#0").addClass("past");
+}
+else {
+    $("#0").addClass("future");
+}
+
+//10
+
+
+
+
+
